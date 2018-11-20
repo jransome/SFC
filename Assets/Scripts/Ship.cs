1c11746036c288a75cb76ccc627e896b8ccac882
James Ransome <jamesransome@msn.com>
2018-11-20 22:12:49 +0000

    Extract TargetingSystem, need to update views

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -1,63 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Targetable))]
 public class Ship : MonoBehaviour
 {
     public GameObject StatusUIPrefab;
-    public List<Targetable> VisibleTargets = new List<Targetable>();
 
     private MountedWeapon[] mountedWeapons;
     private Targetable self;
-    private Targetable target;
-
-    public event Action<Targetable> TargetChanged = delegate { }; 
 
     public List<Hardpoint> Hardpoints { get; private set; }
     public Shields Shields { get; private set; }
     public Engines Engines { get; private set; }
-    public Targetable Target
-    {
-        get { return target ? target : null; }
-        private set
-        {
-            if (value == target) return;
-            target = value;
-            TargetChanged(target);
-        }
-    }
-    public float? TargetHeading
-    {
-        get
-        {
-            if (target == null) return null;
-            return Helpers.CalculateHorizonHeading(transform.forward, target.Position - transform.position);
-        }
-    }
+    public TargetingSystem TargetingSystem { get; private set; }
 
-    public float? TargetRelativeHeading() // Returns the heading of this ship from the target's perspective
-    {
-        if (target == null) return null;
-        return target.CalcRelativeHeading(transform.position);
-    }
-
-    public float CurrentHealth
-    {
-        get { return self.CurrentHealth; }
-    }
-
-    public void SetTarget(Targetable newTarget)
-    {
-        Target = newTarget;
-        foreach (var weapon in mountedWeapons)
-            weapon.Target = Target;
-    }
-
-    public void ClearTarget()
-    {
-        SetTarget(null);
-    }
+    public float CurrentHealth => self.CurrentHealth;
 
     private void Awake()
     {
@@ -65,6 +22,7 @@ public class Ship : MonoBehaviour
         Hardpoints = new List<Hardpoint>(GetComponentsInChildren<Hardpoint>());
         Shields = GetComponentInChildren<Shields>();
         Engines = GetComponent<Engines>();
+        TargetingSystem = GetComponent<TargetingSystem>();
         mountedWeapons = GetComponentsInChildren<MountedWeapon>();
     }
 }
---
+++ Assets/Scripts/TargetingSystem.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetingSystem : MonoBehaviour
+{
+    private bool isUpdatingFacings = true;
+    private Targetable target;
+    private Facing targetFacing;
+    private Facing targetRelativeFacing;
+
+    public event Action<Targetable> TargetChanged = delegate { };
+    public event Action<Facing> TargetFacingChanged = delegate { };
+    public event Action<Facing> TargetRelativeFacingChanged = delegate { };
+
+    public List<Targetable> VisibleTargets = new List<Targetable>();
+    public Targetable Target
+    {
+        get { return target ? target : null; }
+        private set
+        {
+            if (value == target) return;
+            if (value == null)
+            {
+                isUpdatingFacings = false;
+            }
+            else
+            {
+                isUpdatingFacings = true;
+                StartCoroutine(UpdateTargetFacings());
+            }
+            target = value;
+            TargetChanged(target);
+        }
+    }
+    public Facing TargetFacing
+    {
+        get { return target ? targetFacing : null; }
+        private set
+        {
+            if (value == targetFacing) return;
+            targetFacing = value;
+            TargetFacingChanged(value);
+        }
+    }
+    public Facing TargetRelativeFacing // Returns the facing of this ship from the target's perspective
+    {
+        get { return target ? targetRelativeFacing : null; }
+        private set
+        {
+            if (value == targetRelativeFacing) return;
+            targetRelativeFacing = value;
+            TargetRelativeFacingChanged(value);
+        }
+    }
+
+    public void SetTarget(Targetable newTarget, MountedWeapon[] mountedWeapons) // TODO move out weapons stuff
+    {
+        Target = newTarget;
+        foreach (var weapon in mountedWeapons)
+            weapon.Target = Target;
+    }
+
+    public void ClearTarget(MountedWeapon[] mountedWeapons)
+    {
+        SetTarget(null, mountedWeapons);
+    }
+
+    private IEnumerator UpdateTargetFacings()
+    {
+        while (isUpdatingFacings)
+        {
+            float targetHeading = Helpers.CalculateHorizonHeading(transform.forward, target.Position - transform.position);
+            TargetFacing = Facing.GetFacingByHeading(targetHeading);
+
+            float targetRelativeHeading = target.CalcRelativeHeading(transform.position);
+            TargetRelativeFacing = Facing.GetFacingByHeading(targetRelativeHeading);
+
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
+}
